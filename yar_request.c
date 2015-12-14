fe9975178abb2df847300bc16c7f78e8ee9d0344
Albert <lb13810398408@gmail.com>
2015-12-14 17:21:34 +0800

    test @public

--- yar_request.c
+++ yar_request.c
@@ -235,24 +235,6 @@ NOTE:   String length must be evenly divisible by 16byte (str_len % 16 == 0)
 #endif
 
 
-
-#if defined(ECB) && ECB
-
-void AES128_ECB_encrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
-void AES128_ECB_decrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
-
-#endif // #if defined(ECB) && ECB
-
-
-#if defined(CBC) && CBC
-
-void AES128_CBC_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
-void AES128_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
-
-#endif // #if defined(CBC) && CBC
-
-
-
 #endif //_AES_H_
 
 
